ac9d7b2c6669d27d54b9fbe8473c0d7e7c093ef0
sumant murke <sumantmurke@gmail.com>
2014-09-26 01:06:07 -0700

    User DTO
    User object created with setters and getters

---
+++ src/main/java/com/ezzyBuy/Domain/User.java
@@ -0,0 +1,51 @@
+package com.ezzyBuy.Domain;
+
+public class User {
+	private String password;
+	private String userName;
+	private String firstName;
+	private String lastName;
+	private String emailId;
+	private String creditCard;
+	
+	
+	
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getCreditCard() {
+		return creditCard;
+	}
+	public void setCreditCard(String creditCard) {
+		this.creditCard = creditCard;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+}
